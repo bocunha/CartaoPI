9b61b6ff72609778d3a19a3e01b48669dce413db
BRUNO ORTALE CUNHA <987324141@itaud.des.ihf>
2020-07-14 09:46:13 -0300

    Corrigindo Comentarios

--- src/model/CartaoCashBack.java
+++ src/model/CartaoCashBack.java
@@ -3,6 +3,7 @@ package model;
 public class CartaoCashBack extends CartaoPrePago{
 	protected int tipo;
 
+		// Teste usando vetores diretos
 	String[] nomeTipos = {"", "BRONZE","SILVER","GOLD"};
 	Double[] pctTipos = {0.0, 0.02, 0.05, 0.08};
 			 //tipos	0,		1	,2		3;
@@ -18,20 +19,25 @@ public class CartaoCashBack extends CartaoPrePago{
 	
 		public boolean comprar(double valor) {
 			if (super.saldo >= valor) {
-				
-				super.saldo -= valor;
-				super.saldo += valor*(pctTipos[tipo]);
 
-//				switch (tipo) {O
+//*************** Teste chamando o vetor direto nessa classe
+				super.saldo -= valor;
+				super.saldo += valor*(this.pctTipos[this.tipo]);
+
+//**************** Primeiro teste usando switch case
+//				switch (this.tipo) {
 //					case 1: super.saldo -= valor; super.saldo += valor*0.02; break;
 //					case 2: super.saldo -= valor; super.saldo += valor*0.05; break;
 //					case 3: super.saldo -= valor; super.saldo += valor*0.08; break; }
+//**************** Segundo teste usando a class Tipo (tipo.java)
 //				Tipo tipo[];
 //				tipo = new Tipo[4];
 //				tipo[0] = new Tipo(1,0.02);
 //				tipo[1] = new Tipo(1,0.02);
 //				tipo[2] = new Tipo(2,0.05);
 //				tipo[3] = new Tipo(3,0.08);
+//				super.saldo -= valor;
+//				super.saldo += valor*(tipo[this.tipo].getPct());
 				
 				return true;
 				} else { 
@@ -51,4 +57,3 @@ public class CartaoCashBack extends CartaoPrePago{
 					+"\nSaldo: "+ +this.saldo);			
 		}
 }
-
