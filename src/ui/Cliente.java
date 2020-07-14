a85a12aae57749bb4454a45c3d23b77e7dc06125
bocunha <bocunha@hotmail.com>
2020-07-14 15:17:44 -0300

    Arraylist

--- src/ui/Cliente.java
+++ src/ui/Cliente.java
@@ -1,5 +1,7 @@
 package ui;
 
+import java.util.ArrayList;
+
 import javax.swing.JOptionPane;
 
 import model.CartaoCashBack;
@@ -9,12 +11,20 @@ public class Cliente {
 
 	public static void main(String[] args) {
 	    
-		CartaoPrePago cartoes[];
-		cartoes = new CartaoPrePago[4];
-		cartoes[0] = new CartaoPrePago("1111122233334444","Professor Isidrao",2020,6);
-		cartoes[1] = new CartaoCashBack("2222333344445555","Professor Pardal",2021,2,1);
-		cartoes[2] = new CartaoCashBack("2222333344445555","Professor Roberto",2022,2,2);
-		cartoes[3] = new CartaoCashBack("2222333344445555","Professor Girafales",2023,2,3);
+		ArrayList<CartaoPrePago> cartoes;
+		cartoes = new ArrayList<CartaoPrePago>();
+		cartoes.add(new CartaoPrePago("1111122233334444","Professor Isidrao",2020,6));
+		cartoes.add(new CartaoCashBack("2222333344445555","Professor Pardal",2021,2,1));
+		cartoes.add(new CartaoCashBack("2222333344445555","Professor Roberto",2022,2,2));
+		cartoes.add(new CartaoCashBack("2222333344445555","Professor Girafales",2023,2,3));
+
+		
+//		CartaoPrePago cartoes[];
+//		cartoes = new CartaoPrePago[4];
+//		cartoes[0] = new CartaoPrePago("1111122233334444","Professor Isidrao",2020,6);
+//		cartoes[1] = new CartaoCashBack("2222333344445555","Professor Pardal",2021,2,1);
+//		cartoes[2] = new CartaoCashBack("2222333344445555","Professor Roberto",2022,2,2);
+//		cartoes[3] = new CartaoCashBack("2222333344445555","Professor Girafales",2023,2,3);
 
 		
 		for (CartaoPrePago c : cartoes ) {
